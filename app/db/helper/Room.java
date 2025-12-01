9bac81f81226f79d9c796d36099a31215654817f
wrenverse <wrenverse@outlook.com>
2025-12-01 02:33:41 -0500

    feat(db): add room information get functions

--- app/db/helper/Room.java
+++ app/db/helper/Room.java
@@ -4,22 +4,41 @@ import java.sql.*;
  * Helper class to handle the rooms table in the database.
  */
 public class Room {
-    
+
     /**
-     * Get the information of a room by ID.
+     * Get the location of a room by ID.
      * @param conn The connection to the database.
-     * @param roomId The ID of the trainer.
-     * @return The table row containing the information.
+     * @param roomId The ID of the room.
+     * @return The location of the room.
      */
-    public static ResultSet getInfo(Connection conn, Integer roomId) {
+    public static String getLocation(Connection conn, Integer roomId) {
         try {
-            String query = "SELECT * FROM rooms WHERE room_id = ?";
+            if (!exists(conn, roomId)) return null;
+            String query = "SELECT location FROM rooms WHERE room_id = ?";
             PreparedStatement pstmt = conn.prepareStatement(query);
             pstmt.setInt(1, roomId);
-            return pstmt.executeQuery(query);
+            return pstmt.executeQuery(query).getString("location");
         } catch (Exception e) {
             Terminal.exception(e);
         }
         return null;
+    }
+
+    /**
+     * Check if a room exists with the given ID.
+     * @param conn The connection to the database.
+     * @param roomId The ID of the room.
+     * @return True if the room exists, false otherwise.
+     */
+    public static boolean exists(Connection conn, Integer roomId) {
+        try {
+            String query = "SELECT room_id FROM rooms WHERE room_id = ?";
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setInt(1, roomId);
+            return pstmt.executeQuery(query).next();
+        } catch (Exception e) {
+            Terminal.exception(e);
+        }
+        return false;
     }
 }
