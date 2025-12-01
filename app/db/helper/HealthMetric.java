7ad5280b451d0300dccea58062d9d84e61955f73
wrenverse <wrenverse@outlook.com>
2025-12-01 16:32:25 -0500

    feat(db): add health metric record information get functions

--- app/db/helper/HealthMetric.java
+++ app/db/helper/HealthMetric.java
@@ -1,5 +1,6 @@
 import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.LinkedList;
 
 /**
  * Helper class to handle the health metrics table in the database.
@@ -53,15 +54,120 @@ public class HealthMetric {
     }
 
     /**
-     * Get the latest recorded health metric record of a member by ID.
+     * Get the timestamp of a health metric record by ID.
+     * @param conn The connection to the database.
+     * @param metricId The ID of the health metric record.
+     * @return The timestamp of the record.
+     */
+    public static Timestamp getTimestamp(Connection conn, Integer metricId) {
+        try {
+            if (!exists(conn, metricId)) return null;
+            String query = "SELECT timestamp FROM health_metrics WHERE metric_id = ?";
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setInt(1, metricId);
+            Timestamp ts = pstmt.executeQuery(query).getTimestamp("timestamp");
+            pstmt.close();
+            return ts;
+        } catch (Exception e) {
+            Terminal.exception(e);
+        }
+        return null;
+    }
+
+    /**
+     * Get the heart rate of a health metric record by ID.
+     * @param conn The connection to the database.
+     * @param metricId The ID of the health metric record.
+     * @return The heart rate of the record.
+     */
+    public static Integer getHeartRate(Connection conn, Integer metricId) {
+        try {
+            if (!exists(conn, metricId)) return null;
+            String query = "SELECT heart_rate FROM health_metrics WHERE metric_id = ?";
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setInt(1, metricId);
+            Integer hr = pstmt.executeQuery(query).getInt("heart_rate");
+            pstmt.close();
+            return hr;
+        } catch (Exception e) {
+            Terminal.exception(e);
+        }
+        return null;
+    }
+
+    /**
+     * Get the body fat percentage of a health metric record by ID.
+     * @param conn The connection to the database.
+     * @param metricId The ID of the health metric record.
+     * @return The body fat percentage of the record.
+     */
+    public static Float getBodyFat(Connection conn, Integer metricId) {
+        try {
+            if (!exists(conn, metricId)) return null;
+            String query = "SELECT body_fat FROM health_metrics WHERE metric_id = ?";
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setInt(1, metricId);
+            Float bfp = pstmt.executeQuery(query).getFloat("body_fat");
+            pstmt.close();
+            return bfp;
+        } catch (Exception e) {
+            Terminal.exception(e);
+        }
+        return null;
+    }
+
+    /**
+     * Get the weight of a health metric record by ID.
+     * @param conn The connection to the database.
+     * @param metricId The ID of the health metric record.
+     * @return The weight of the record.
+     */
+    public static Integer getWeight(Connection conn, Integer metricId) {
+        try {
+            if (!exists(conn, metricId)) return null;
+            String query = "SELECT weight FROM health_metrics WHERE metric_id = ?";
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setInt(1, metricId);
+            Integer weight = pstmt.executeQuery(query).getInt("weight");
+            pstmt.close();
+            return weight;
+        } catch (Exception e) {
+            Terminal.exception(e);
+        }
+        return null;
+    }
+
+    /**
+     * Get the height of a health metric record by ID.
+     * @param conn The connection to the database.
+     * @param metricId The ID of the health metric record.
+     * @return The height of the record.
+     */
+    public static Integer getHeight(Connection conn, Integer metricId) {
+        try {
+            if (!exists(conn, metricId)) return null;
+            String query = "SELECT height FROM health_metrics WHERE metric_id = ?";
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setInt(1, metricId);
+            Integer height = pstmt.executeQuery(query).getInt("height");
+            pstmt.close();
+            return height;
+        } catch (Exception e) {
+            Terminal.exception(e);
+        }
+        return null;
+    }
+
+    /**
+     * Get the ID of the latest recorded health metric record of a member by ID.
      * @param conn The connection to the database.
      * @param memberId The ID of the member.
-     * @return The table row containing the latest record.
+     * @return The ID of the the latest record.
      */
-    public static ResultSet getLatest(Connection conn, Integer memberId) {
+    public static Integer getLatest(Connection conn, Integer memberId) {
         try {
             String query = """
-                SELECT * FROM health_metrics
+                SELECT metric_id FROM health_metrics
                     WHERE timestamp =
                         (SELECT MAX(timestamp)
                             FROM health_metrics
@@ -71,7 +177,9 @@ public class HealthMetric {
             PreparedStatement pstmt = conn.prepareStatement(query);
             pstmt.setInt(1, memberId);
             pstmt.setInt(2, memberId);
-            return pstmt.executeQuery(query);
+            Integer id = pstmt.executeQuery(query).getInt("metric_id");
+            pstmt.close();
+            return id;
         } catch (Exception e) {
             Terminal.exception(e);
         }
@@ -79,20 +187,45 @@ public class HealthMetric {
     }
 
     /**
-     * Get the health metric records of a member by ID.
+     * Get the IDs of health metrics records of a member by ID.
      * @param conn The connection to the database.
      * @param memberId The ID of the member.
-     * @return The table rows containing the records.
+     * @return The IDs of the health metric records.
      */
-    public static ResultSet getRecords(Connection conn, Integer memberId) {
+    public static LinkedList<Integer> getRecords(Connection conn, Integer memberId) {
         try {
-            String query = "SELECT * FROM health_metrics WHERE member_id = ?";
+            String query = "SELECT metric_id FROM health_metrics WHERE member_id = ?";
             PreparedStatement pstmt = conn.prepareStatement(query);
             pstmt.setInt(1, memberId);
-            return pstmt.executeQuery(query);
+            ResultSet rs = pstmt.executeQuery(query);
+            LinkedList<Integer> ids = new LinkedList<>();
+            while (rs.next()) ids.add(rs.getInt("metric_id"));
+            pstmt.close();
+            rs.close();
+            return ids;
         } catch (Exception e) {
             Terminal.exception(e);
         }
         return null;
+    }
+
+    /**
+     * Check if a health metric record exists with the given ID.
+     * @param conn The connection to the database.
+     * @param metricId The ID of the health metric record.
+     * @return True if the record exists, false otherwise.
+     */
+    public static boolean exists(Connection conn, Integer metricId) {
+        try {
+            String query = "SELECT metric_id FROM health_metrics WHERE metric_id = ?";
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setInt(1, metricId);
+            boolean exists = pstmt.executeQuery(query).next();
+            pstmt.close();
+            return exists;
+        } catch (Exception e) {
+            Terminal.exception(e);
+        }
+        return false;
     }
 }
