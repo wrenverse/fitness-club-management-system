58b013b458144eb48162779a46780c43dbf61060
wrenverse <wrenverse@outlook.com>
2025-12-01 17:23:03 -0500

    feat(db): add class registration information get functions

--- app/db/helper/ClassRegistration.java
+++ app/db/helper/ClassRegistration.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.LinkedList;
 
 /**
  * Helper class to handle the class registration table in the database.
@@ -123,5 +124,106 @@ public class ClassRegistration {
             return true;
         }
         return false;
+    }
+
+    /**
+     * Check if a member is registers in a class with the given IDs.
+     * @param conn The connection to the database.
+     * @param classId The ID of class.
+     * @param memberId The ID of the member.
+     * @return True if the member is registered in the class, false otherwise.
+     */
+    public static boolean isRegistered(Connection conn, Integer classId, Integer memberId) {
+        try {
+            String query = """
+                SELECT *
+                    FROM class_registration
+                    WHERE class_id = ?
+                        AND member_id = ?
+                """;
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setInt(1, classId);
+            pstmt.setInt(2, memberId);
+            boolean isRegistered = pstmt.executeQuery(query).next();
+            pstmt.close();
+            return isRegistered;
+        } catch (Exception e) {
+            Terminal.exception(e);
+        }
+        return false;
+    }
+
+    /**
+     * Get the date a member registered to a class.
+     * @param conn The connection to the database.
+     * @param classId The ID of class.
+     * @param memberId The ID of the member.
+     * @return The registration date of the member registering to the class.
+     */
+    public static Date getRegistrationDate(Connection conn, Integer classId, Integer memberId) {
+        try {
+            if (!isRegistered(conn, classId, memberId)) return null;
+            String query = """
+                SELECT register_date
+                    FROM class_registration
+                    WHERE class_id = ?
+                        AND member_id = ?
+                """;
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setInt(1, classId);
+            pstmt.setInt(2, memberId);
+            Date date = pstmt.executeQuery(query).getDate("register_date");
+            pstmt.close();
+            return date;
+        } catch (Exception e) {
+            Terminal.exception(e);
+        }
+        return null;
+    }
+
+    /**
+     * Get the IDs of registered classes of a member by ID.
+     * @param conn The connection to the database.
+     * @param memberId The ID of the member.
+     * @return The IDs of the registered classes.
+     */
+    public static LinkedList<Integer> getRegistratedClasses(Connection conn, Integer memberId) {
+        try {
+            String query = "SELECT class_id FROM class_registration WHERE member_id = ?";
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setInt(1, memberId);
+            ResultSet rs = pstmt.executeQuery(query);
+            LinkedList<Integer> ids = new LinkedList<>();
+            while (rs.next()) ids.add(rs.getInt("class_id"));
+            pstmt.close();
+            rs.close();
+            return ids;
+        } catch (Exception e) {
+            Terminal.exception(e);
+        }
+        return null;
+    }
+
+    /**
+     * Get the IDs of registered students of a class by ID.
+     * @param conn The connection to the database.
+     * @param classId The ID of the class.
+     * @return The IDs of the registered members.
+     */
+    public static LinkedList<Integer> getRegisteredMembers(Connection conn, Integer classId) {
+        try {
+            String query = "SELECT member_id FROM class_registration WHERE class_id = ?";
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setInt(1, classId);
+            ResultSet rs = pstmt.executeQuery(query);
+            LinkedList<Integer> ids = new LinkedList<>();
+            while (rs.next()) ids.add(rs.getInt("member_id"));
+            pstmt.close();
+            rs.close();
+            return ids;
+        } catch (Exception e) {
+            Terminal.exception(e);
+        }
+        return null;
     }
 }
